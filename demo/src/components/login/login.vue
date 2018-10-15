29583d6e7ca30b1bd76bf7edafb00c003ac1f1a1
WuLiJun2116@163.com <WuLiJun2116@163.com>
2018-10-15 17:44:18 +0800

    暂存

--- demo/src/components/login/login.vue
+++ demo/src/components/login/login.vue
@@ -1,3 +1,15 @@
 <template>
-	<h1>登录/注册</h1>
+    <h1>登录/注册</h1>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
+
+<script>
+import HelloWorld from '../components/HelloWorld.vue'
+
+export default {
+    name: 'login',
+    components: {
+        HelloWorld
+    }
+}
+</script>
--- demo/src/main.js
+++ demo/src/main.js
@@ -1,16 +1,19 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from './App.vue'
-import routes from './router/router'
+// import routes from './router/router'
+import store from './store'
+import router from './router'
 
 Vue.use(VueRouter)
-const router = new VueRouter({
-	routes
-})
+// const router = new VueRouter({
+// 	routes
+// })
 Vue.config.productionTip = false
 
 new Vue({
-  // store
   render: h => h(App),
+  store,
+  // router,
   router
 }).$mount('#app')
